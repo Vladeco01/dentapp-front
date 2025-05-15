bb438c340ddd51a8092a01616e6785ca5097f340
vladecok23 <vlad.ecobescu@kontron.ro>
2025-05-15 16:52:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AuthenticationPage from "./components/AuthenticationPage";
+import AppointmentsPage from "./components/AppointmentsPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className="container mt-5">
-        <Routes>
-          <Route path="/authenticate" element={<AuthenticationPage />} />
-          <Route path="/appointments" element={<div>Appointments Page</div>} />
-          <Route path="/clinics" element={<div>Clinics Page</div>} />
+          <Routes>
+            <Route path="/authenticate" element={<AuthenticationPage />} />
+            <Route path="/appointments" element={<AppointmentsPage />} />
+           <Route path="/clinics" element={<div>Clinics Page</div>} />
         </Routes>
-      </div>
-    </Router>
+</Router>
+
   );
 }
 
