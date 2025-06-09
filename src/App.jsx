3f88aeab87e45e297896a40dfc41cee531c73c1c
Vlad Ecobescu <vlad_andrey1@yahoo.com>
2025-06-09 23:06:54 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./header/Header";
+import { Routes, Route, Router } from "react-router-dom";
 import AuthenticationPage from "./components/AuthenticationPage";
-
+import Header from "./header/Header";
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Header />
       <div className="container mt-5">
         <Routes>
           <Route path="/authenticate" element={<AuthenticationPage />} />
